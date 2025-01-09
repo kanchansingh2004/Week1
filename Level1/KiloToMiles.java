58c4d92d800a9938d4395563730a40d3c95212a6
kanchansingh2004 <kannursingh2004@gmail.com>
2025-01-09 16:54:23 +0530

    Converting Kilometers to miles

---
+++ Level1/KiloToMiles.java
@@ -0,0 +1,18 @@
+// Creating Kilo_To_Miles Class to convert the value from kilometer to miles.
+
+ class KiloToMiles{
+	 
+	public static void main(String args[])
+	{
+		// Create a variable to initialize the value in kilometer .
+		float distanceInKm=10.8f;
+		
+		// Create a variable to convert the distance in km to miles.
+		float distanceInMiles=distanceInKm*1.6f;
+		
+		
+		// Printing the average marks of Sam
+		System.out.println(" The distance "+ distanceInKm + "km in miles is " + distanceInMiles);
+		
+	}
+}
